8ab064c27ec5f2ea25ca66883a5ffcb7d1a1a0f4
ilian1902 <ilian.bratanov82@gmail.com>
2017-01-31 19:52:26 +0200

    Initial methods in the GeofenceService.js

--- app/services/GeofenceService.js
+++ app/services/GeofenceService.js
@@ -1,3 +1,26 @@
-export class GeofenceService {
+const GEOFENCE_RADIUS = 10;
 
+class GeofenceService {
+
+    constructor() {}
+
+    geofenceEnter(chain, store) {
+
+    }
+
+    geofenceExit(chain, store) {
+
+    }
+
+    initialize(sproximityMarketingData) {
+
+    }
+
+    intializeApi() {
+
+    }
+
+    addGeofence(chain, store) {
+
+    }
 }
