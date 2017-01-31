5fea376b4b7c030a65c42a69063260e08f623d6e
VenelinGP <venelin.petkov@netcoms.eu>
2017-01-31 11:02:41 +0200

    Added circle with radius and created object "Obecto".

--- app/map-page.js
+++ app/map-page.js
@@ -1,20 +1,38 @@
 //  /app/main-page.js
 
 var mapsModule = require("nativescript-google-maps-sdk");
+var Color = require("color").Color;
+
+let obecto = {
+    name: "Obecto",
+    city: "Sofia",
+    latitude: 42.66251948309662,
+    longitude: 23.31786286085844,
+    radius: 11
+};
 
 function onMapReady(args) {
     var mapView = args.object;
 
     console.log("Setting a marker...");
     var marker = new mapsModule.Marker();
-    marker.position = mapsModule.Position.positionFromLatLng(42.66251948309662, 23.31786286085844);
-    marker.title = "Obecto";
-    marker.snippet = "Sofia";
+    marker.position = mapsModule.Position.positionFromLatLng(obecto.latitude, obecto.longitude);
+    marker.title = obecto.name;
+    marker.snippet = obecto.city;
     marker.userData = { index: 1 };
-    mapView.latitude = 42.66251948309662;
-    mapView.longitude = 23.31786286085844;
-    mapView.zoom = 22;
+    mapView.latitude = obecto.latitude;
+    mapView.longitude = obecto.longitude;
+    mapView.zoom = 20;
     mapView.addMarker(marker);
+
+    var circle = new mapsModule.Circle();
+    circle.center = mapsModule.Position.positionFromLatLng(obecto.latitude, obecto.longitude);
+    circle.visible = true;
+    circle.radius = obecto.radius;
+    circle.fillColor = new Color('#997faaff');
+    circle.strokeColor = new Color('#992a55ff');
+    circle.strokeWidth = 2;
+    mapView.addCircle(circle);
 }
 
 function onMarkerSelect(args) {
