2b0435edfc9b190836d9494927c2599fc09fa319
ilian1902 <ilian.bratanov82@gmail.com>
2017-02-06 01:11:37 +0200

    Added initialApi method.

--- app/services/GeofenceService.js
+++ app/services/GeofenceService.js
@@ -4,6 +4,11 @@ const GEOFENCE_RADIUS = 10;
 const GoogleApi = com.google.android.gms.location;
 const context = utils.ad.getApplicationContext();
 
+const GoogleApiClient = com.google.android.gms.common.api.GoogleApiClient;
+const LocationServices = com.google.android.gms.location.LocationServices;
+const LocationRequest = com.google.android.gms.location.LocationRequest;
+const LocationListener = com.google.android.gms.location.LocationListener;
+
 class GeofenceService {
 
     constructor() {}
@@ -21,7 +26,40 @@ class GeofenceService {
     }
 
     intializeApi() {
+        this.googleApiClient = new GoogleApiClient.Builder(application.android.context)
+            .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks({
+                onConnected: function() {
+                    debug('onConnected', arguments);
+                    this.ready = true;
+                    this.notify({
+                        eventName: 'ready',
+                    });
+                    this.notify({
+                        eventName: '_googleApiClientConnected',
+                        object: this.googleApiClient
+                    });
+                }.bind(this),
+                onConnectionSuspended: function() {
+                    debug('onConnectionSuspended', arguments);
+                    this.notify({
+                        eventName: '_googleApiClientConnectionSuspended',
+                        object: this.googleApiClient
+                    });
+                }.bind(this),
+            }))
+            .addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener({
+                onConnectionFailed: function() {
+                    debug('onConnectionFailed', arguments);
+                    this.notify({
+                        eventName: '_googleApiClientConnectionFailed',
+                        object: this.googleApiClient
+                    });
+                }.bind(this),
+            }))
+            .addApi(LocationServices.API)
+            .build();
 
+        this.googleApiClient.connect();
     }
 
     addGeofence(chain, store) {
