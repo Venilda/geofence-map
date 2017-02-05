824368b116d0c296574cd9be65c8f54b03b92431
ilian1902 <ilian.bratanov82@gmail.com>
2017-02-05 19:40:16 +0200

    Added addGeofence method.

--- app/services/GeofenceService.js
+++ app/services/GeofenceService.js
@@ -1,4 +1,8 @@
+var utils = require("utils/utils");
+
 const GEOFENCE_RADIUS = 10;
+const GoogleApi = com.google.android.gms.location;
+const context = utils.ad.getApplicationContext();
 
 class GeofenceService {
 
@@ -13,7 +17,7 @@ class GeofenceService {
     }
 
     initialize(sproximityMarketingData) {
-
+        return new sproximityMarketingData();
     }
 
     intializeApi() {
@@ -21,6 +25,19 @@ class GeofenceService {
     }
 
     addGeofence(chain, store) {
+        let geofancePoint = new GoogleApi.Geofence.Builder()
+            .setRequestId(chain.name)
+            .setCircularRegion(
+                store.latitude,
+                store.longitude,
+                this.GEOFENCE_RADIUS
+            ).setExpirationDuration(12 * 60 * 60 * 1000)
+            .setTransitionTypes(1 | 2)
+            .build();
 
+        let builder = new GoogleApi.GeofencingRequest.Builder();
+        builder.setInitialTrigger(GoogleApi.GeofencingRequest.INITIAL_TRIGGER_ENTER);
+        builder.addGeofence(geofancePoint);
+        return builder.build();
     }
 }
